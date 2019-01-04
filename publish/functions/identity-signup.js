75bacaf2f888ecfd1979f8b5d2b6a0de195cba1c
Gerald O <gerald.onyango@gmail.com>
2019-01-03 22:45:30 -0500

    adding functions and updating UI

---
+++ publish/functions/identity-signup.js
@@ -0,0 +1,14 @@
+exports.handler = function(event, context, callback) {
+  const responseBody = {
+    app_metadata: {
+      roles: ['test', 'user', 'member'],
+      my_user_info: "this is user info that the user can't change from the UI",
+    },
+    user_metadata: { custom_data_from_ui: 'this is pretty awesome' },
+  };
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(responseBody),
+  });
+};
